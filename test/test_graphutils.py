8db05a82f230b07382c8be1bdbfc1fedc448e33f
KostyaLyman <lyman.kostiantyn@gmail.com>
2022-06-22 11:16:31 -0700

    graph utils

---
+++ test/test_graphutils.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+class TestDistanceUtils():
+    def test_something(self):
+        assert True
+
+
+class TestGraphUtils():
+    def test_something(self):
+        assert False
+
+
